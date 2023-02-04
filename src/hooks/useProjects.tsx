5d73b7f43b96d3c42a63cd8a300303807bc00661
dough <viktor.melin@me.com>
2023-02-04 17:45:54 +0100

    fix: installed eslint dependencies

--- src/hooks/useProjects.tsx
+++ src/hooks/useProjects.tsx
@@ -1,28 +1,28 @@
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-// interface Projects {
-// 	title: string;
-// 	stack: string[];
-// 	repo: string;
-// 	live?: string;
-// }
+interface Projects {
+	title: string;
+	stack: string[];
+	repo: string;
+	live?: string;
+}
 
-// interface Data {
-// 	loading: boolean;
-// 	data: Projects[];
-// }
+interface Data {
+	loading: boolean;
+	data: Projects[];
+}
 
-// const useProjects = () => {
-// 	const [data, setData] = useState<Data>({
-// 		loading: true,
-// 		data: [],
-// 	});
+const useProjects = () => {
+	const [data, setData] = useState<Data>({
+		loading: true,
+		data: [],
+	});
 
-// 	useEffect(() => {
-// 		setData();
-// 	}, []);
+	// useEffect(() => {
+	// 	setData();
+	// }, []);
 
-// 	return data;
-// };
+	return data;
+};
 
-// export default useProjects;
+export default useProjects;
