a16ad08da326bc4f108425856f93bc450706ec22
dough <viktor.melin@me.com>
2023-03-09 13:23:54 +0100

    feat: add updated portfolio objects

--- src/store/projects.ts
+++ src/store/projects.ts
@@ -1,26 +1,26 @@
 const projects = [
-	{
-		title: 'paste(bin)',
-		stack: ['TypeScript', 'NextJS', 'Mantine', 'Prisma', 'PostgreSQL'],
-		repo: 'https://github.com/viktormelin/dixxel-bin',
-		live: 'https://bin.dixxel.io/',
-		image:
-			'https://cdn.dixxel.io/cdn-cgi/imagedelivery/3ecvmLCFkS-FijMWb0qFvQ/71efec5b-4832-4726-2128-b581cf2e7900/public',
-	},
-	{
-		title: 'Images',
-		stack: ['TypeScript', 'NextJS', 'Mantine', 'Prisma', 'PostgreSQL'],
-		repo: 'https://github.com/viktormelin/cf-images',
-		live: 'https://images.dixxel.io/',
-		image:
-			'https://cdn.dixxel.io/cdn-cgi/imagedelivery/3ecvmLCFkS-FijMWb0qFvQ/d3ff7060-41af-45dc-afcc-b80a30cd0600/public',
-	},
-	{
-		title: 'Movies',
-		stack: ['JavaScript', 'React', 'MaterialUi', 'Redux', 'Express', 'MongoDB', 'MongoDB', 'MongoDB'],
-		repo: 'https://github.com/viktormelin/movie-dashboard',
-		live: '',
-	},
+  {
+    title: 'Movies',
+    stack: ['TypeScript', 'NextJS', 'Mantine', 'Prisma', 'PostgreSQL'],
+    repo: 'https://github.com/viktormelin/movies',
+    live: '',
+  },
+  {
+    title: 'paste(bin)',
+    stack: ['TypeScript', 'NextJS', 'Mantine', 'Prisma', 'PostgreSQL'],
+    repo: 'https://github.com/viktormelin/dixxel-bin',
+    live: 'https://bin.dixxel.io/',
+    image:
+      'https://cdn.dixxel.io/cdn-cgi/imagedelivery/3ecvmLCFkS-FijMWb0qFvQ/71efec5b-4832-4726-2128-b581cf2e7900/public',
+  },
+  {
+    title: 'Images',
+    stack: ['TypeScript', 'NextJS', 'Mantine', 'Prisma', 'PostgreSQL'],
+    repo: 'https://github.com/viktormelin/cf-images',
+    live: 'https://images.dixxel.io/',
+    image:
+      'https://cdn.dixxel.io/cdn-cgi/imagedelivery/3ecvmLCFkS-FijMWb0qFvQ/d3ff7060-41af-45dc-afcc-b80a30cd0600/public',
+  },
 ];
 
 export default projects;
